00d2c0960a584d9c4be11df7dce2bceefb5c873e
Leandro Alsberg <leandro.alsberg@gmail.com>
2019-08-19 13:50:23 -0300

    WIP hanoi linuxcc

--- hanoi/src/hanoi/core.clj
+++ hanoi/src/hanoi/core.clj
@@ -1,25 +1,40 @@
 (ns hanoi.core
   (:gen-class))
 
+(def empty-cell {:text "  |  " :weight 0})
+
 (def field 
-	[[{:text "  -  " :weight 1} {:text "  |  " :weight 0} {:text "  |  " :weight 0}]
-	 [{:text " --- " :weight 2} {:text "  |  " :weight 0} {:text "  |  " :weight 0}]
-	 [{:text "-----" :weight 3} {:text "  |  " :weight 0} {:text "  |  " :weight 0}]])
+	[[{:text "  -  " :weight 1} empty-cell empty-cell]
+	 [{:text " --- " :weight 2} empty-cell empty-cell]
+	 [{:text "-----" :weight 3} empty-cell empty-cell]])
 
 (def field-base-index
 	(- (count field) 1))
 
-(def get-top
+(defn get-column
 	[column]
-	
+	(for [line (range 0 (+ field-base-index 1))]
+		((field line) column))
 	)
+
+(defn has-piece
+	[cell]
+	(> (:weight cell) 0))
+
+(defn get-top
+	[column]
+
+	(first
+		(into []
+			(filter has-piece (get-column column)))))
 
 (defn can-move
 	[origin destiny]
-	; (or 
-	; 	(:weight ((field 2) destiny))
-	; 	)
+	(or 
+		(:weight ((field 2) destiny))
+		)
 	)
+
 (defn move
 	[origin destiny]
 	(if (can-move origin destiny)
@@ -34,5 +49,7 @@
   (println (:text ((field 0) 0)))
   (println (:weight ((field 0) 1)))
   (println field-base-index)
+  (println (get-column 1))
+  (println (get-top 0))
   ; (println (move 1 2))
   )
